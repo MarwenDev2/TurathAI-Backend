f4c87dfe5698d6d951b1a4e4f628f0e0c1e67593
MariemSaidi7 <saidi.mariem@esprit.tn>
2025-03-23 07:25:24 +0100

    Added crud + few ops for Review entity

--- src/main/java/pi/turathai/turathaibackend/Entites/Review.java
+++ src/main/java/pi/turathai/turathaibackend/Entites/Review.java
@@ -1,6 +1,8 @@
 package pi.turathai.turathaibackend.Entites;
+
 import jakarta.persistence.*;
 import lombok.*;
+
 
 import java.sql.Date;
 
@@ -9,20 +11,30 @@ import java.sql.Date;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"idUser", "idSite"}) // Ensure a unique combination of user and heritage site
+})
 public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // Primary key
 
     private int rating;
     private String comment;
     private Date createdAt;
+    private boolean flagged;  // Attribute to flag inappropriate reviews
 
-    @Id
+    @PrePersist
+    protected void onCreate() {
+        createdAt = new Date(System.currentTimeMillis());
+    }
+
     @ManyToOne
     @JoinColumn(name = "idUser")
     private User user;
 
-    @Id
     @ManyToOne
     @JoinColumn(name = "idSite")
     private HeritageSite heritageSite;
-
 }
